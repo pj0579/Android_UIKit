1faa55b8fe16ce60eb898b4e9977b37c8b6f4040
xukankan <1095584555@qq.com>
2020-04-15 10:49:31 +0800

    图片

--- app/src/main/java/com/github/xkk/android_uikit/main/MainActivity.java
+++ app/src/main/java/com/github/xkk/android_uikit/main/MainActivity.java
@@ -1,12 +1,24 @@
 package com.github.xkk.android_uikit.main;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.net.Uri;
+import android.os.IBinder;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.github.xkk.android_uikit.IMyAidlInterface;
 import com.github.xkk.android_uikit.R;
 import com.github.xkk.android_uikit.common.BaseRecyclerViewAdapter;
 import com.github.xkk.android_uikit.main.card_views.CardPagerActivity;
@@ -49,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
         list.add("子View params对自己mode的影响");
         list.add("优酷首页高清板块切换效果");
         list.add("网易云切换(照着自定义-移花接木撸)");
+        list.add("大图加载");
         mainRecyclerViewAdapter = new MainRecyclerViewAdapter(list, this, R.layout.list_item);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(mainRecyclerViewAdapter);
@@ -67,6 +80,8 @@ public class MainActivity extends AppCompatActivity {
                     case 2:
                         Intent wangyiyun = new Intent(MainActivity.this, WangyiyunActivity.class);
                         startActivity(wangyiyun);
+                        break;
+                    case 3:
                         break;
                 }
             }
