69c476762b58c4f8f629dcd3270ad8e851805a8d
xukankan <1095584555@qq.com>
2020-06-05 19:13:24 +0800

    tag测试

---
+++ app/src/main/java/com/github/xkk/android_uikit/main/nest/NestActivity.java
@@ -0,0 +1,6 @@
+package com.github.xkk.android_uikit.main.nest;
+
+import com.github.xkk.android_uikit.common.BaseActivity;
+
+public class NestActivity extends BaseActivity {
+}
---
+++ app/src/main/java/com/github/xkk/android_uikit/main/nest/StickLayout.java
@@ -0,0 +1,6 @@
+package com.github.xkk.android_uikit.main.nest;
+
+import android.widget.LinearLayout;
+
+public class StickLayout extends LinearLayout {
+}
