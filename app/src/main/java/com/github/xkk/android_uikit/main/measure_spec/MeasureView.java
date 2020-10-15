de89911ddd4a572f1d664c1c285b05dda339ca2c
xukankan <1095584555@qq.com>
2020-10-15 13:45:17 +0800

    test

--- app/src/main/java/com/github/xkk/android_uikit/main/measure_spec/MeasureView.java
+++ app/src/main/java/com/github/xkk/android_uikit/main/measure_spec/MeasureView.java
@@ -19,6 +19,8 @@ public class MeasureView extends View {
         super(context, attrs);
     }
 
+    
+
     public MeasureView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
